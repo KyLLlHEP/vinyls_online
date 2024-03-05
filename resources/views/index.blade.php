b80e224d329fdbca74f97f525e6163c574cae8b5
Artur Kushner <kushnerartur@gmail.com>
2024-03-05 10:00:06 +0000

    feat: rework html file and connect css

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
+    <title>VenylsOnline</title>
+</head>
+<body>
+   <div class="main">
+<div class="header">
+  <div class="logo"></div>
+  <div class="header-items">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Latest</a></li>
+        <li><a href="#">Albums</a></li>
+        <li><a href="#">Singles</a></li>
+        <li><a href="#">Trolly</a></li>
+    </ul>
+    <div class="login">
+        <img src="#" alt="User Avatar">
+        <a href="#">Login/Register</a>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html>
