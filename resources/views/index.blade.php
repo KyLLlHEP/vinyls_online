088100239cc16ea52d67d960d42a47abd30911ee
Artur Kushner <kushnerartur@gmail.com>
2024-03-13 00:34:52 +0000

    feat: add logo and finish header menu ( not finesh header)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,27 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/styles.css">
     <title>VenylsOnline</title>
 </head>
 <body>
-   <div class="main">
-<div class="header">
-  <div class="logo"></div>
-  <div class="header-items">
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Latest</a></li>
-        <li><a href="#">Albums</a></li>
-        <li><a href="#">Singles</a></li>
-        <li><a href="#">Trolly</a></li>
-    </ul>
-    <div class="login">
-        <img src="#" alt="User Avatar">
-        <a href="#">Login/Register</a>
-    </div>
-  </div>
-</div>
-</div>
+    <main>
+        <div class="content-container">
+            <header class="site-header ">
+                <div class="container-header">
+                    <div class="logo-container">
+                        <p class = "logo-txt"> VinylsOnline</p>
+                        <img class = "logo-pic" src="../images/vinyl-record.svg" alt="Vinyls picture">
+                    </div>
+                    <div class="menu-container">
+                        <ul class = "header-menu">
+                            <li class="menu-item"><a class="menu-link" href="#">Home</a></li>
+                            <li class="menu-item"><a class="menu-link" href="#">Latest</a></li>
+                            <li class="menu-item"><a class="menu-link" href="#">Albums</a></li>
+                            <li class="menu-item"><a class="menu-link" href="#">Single</a></li>
+                        </ul>
+                        <div class="cart">
+                            <img src="#" alt="Shopping Trolly">
+                            <span>£350</span>
+                        </div>
+                        <div class="user-avatar">
+                            <img src="#" alt="User Avatar">
+                            <a href="#">Login/Register</a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+            <!-- Здесь будут различные секции вашего сайта -->
+
+            <footer class="site-footer">
+                <!-- Здесь размещаем содержимое подвала сайта -->
+            </footer>
+        </div>
+    </main>
 </body>
+
 </html>
