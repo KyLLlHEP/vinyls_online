71240dd996094ad509a5a80f347fe40eb3ab0d38
Artur Kushner <kushnerartur@gmail.com>
2024-03-19 13:03:38 +0000

    feat: home screen done starte next par choose

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,14 +30,46 @@
                             <span class="cart-price">£350</span>
                         </div>
                         <div class="user-avatar">
-                            <img src="#" alt="User Avatar">
-                            <a href="#">Login/Register</a>
+                            <img  class = "avatar-img" src="../images/avatar.png" alt="User Avatar">
+                            <a class = "login_link" href="#">Login/Register</a>
                         </div>
                     </div>
                 </div>
             </header>
+            <section class = "home-section">
+        <div class="home-container">
+            <div class="description">
+                <h2 class = "description-title">Create a space vibe in<br> your home.</h2>
+                <p class = "description-text">We are a Scottish based online mail order<br>vinyl record shop and also a bricks and<br><br>
+                    mortar high street shop in the centre of<br> Wishaw. We have an extensive stock of 7"<br>singles, LPs,<br><br>
+                    12" singles, limited editions, picture discs<br>and imports.</p>
+                <button class = "find-button" >Find the vinyl</button>
+            </div>
+            <div class="vinyl-image">
+                <img src="../images/home-screen.jpg" alt="Vinyl Record">
+            </div>
+        </div>
+        <section class="choose-section">
+    <h2 class="section-title">Why choose our vinyl records?</h2>
+    <div class="choose-container">
+        <div class="choose-item">
+            <img src="perfect_quality_image.jpg" alt="Perfect Quality">
+            <h3 class="choose-title">Perfect Quality</h3>
+            <p class="choose-description">Our records sound clear and noise-free, all thanks to the highest quality product!</p>
+        </div>
+        <div class="choose-item">
+            <img src="wide_selection_image.jpg" alt="Wide Selection">
+            <h3 class="choose-title">Wide Selection</h3>
+            <p class="choose-description">We offer a wide variety of vinyl records, from classics to the latest releases!</p>
+        </div>
+        <div class="choose-item">
+            <img src="affordable_prices_image.jpg" alt="Affordable Prices">
+            <h3 class="choose-title">Affordable Prices</h3>
+            <p class="choose-description">Our prices are affordable, so you can enjoy your favorite music without breaking the bank!</p>
+        </div>
+    </div>
+</section>
 
-            <!-- Здесь будут различные секции вашего сайта -->
 
             <footer class="site-footer">
                 <!-- Здесь размещаем содержимое подвала сайта -->
